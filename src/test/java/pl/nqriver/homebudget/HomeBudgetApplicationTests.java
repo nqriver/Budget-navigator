46bc6eaeafe1ea613e16d813c68efd7ead2f82c9
nqriver <nqriver@gmail.com>
2022-04-01 01:20:39 +0200

    Initial commit

---
+++ src/test/java/pl/nqriver/homebudget/HomeBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.nqriver.homebudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBudgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
