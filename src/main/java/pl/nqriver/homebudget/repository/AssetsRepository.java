d6a41bd631ba697b52ecf4b4dadc3c98a078ec29
nqriver <nqriver@gmail.com>
2022-04-13 15:14:38 +0200

    Fixing ID type

--- src/main/java/pl/nqriver/homebudget/repository/AssetsRepository.java
+++ src/main/java/pl/nqriver/homebudget/repository/AssetsRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.nqriver.homebudget.repository.entities.AssetEntity;
 
-import java.util.UUID;
-
 @Repository
-public interface AssetsRepository extends JpaRepository<AssetEntity, UUID> {
+public interface AssetsRepository extends JpaRepository<AssetEntity, Long> {
 }
