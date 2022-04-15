1237a202fdffd131efac363ea82adaa9be926da5
nqriver <nqriver@gmail.com>
2022-04-16 00:13:25 +0200

    Adding simple authentication

---
+++ src/main/java/pl/nqriver/homebudget/configurations/AuthenticationBudgetConfiguration.java
@@ -0,0 +1,29 @@
+package pl.nqriver.homebudget.configurations;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import pl.nqriver.homebudget.service.UserDetailsServiceImpl;
+
+@EnableWebSecurity
+public class AuthenticationBudgetConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+
+    public AuthenticationBudgetConfiguration(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
+}
